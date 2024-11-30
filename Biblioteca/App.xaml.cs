4abaa767d6ea8cd981a8581a52765e0b7092da83
Alejandro Tello <89437629+ATelloDev@users.noreply.github.com>
2024-11-29 16:57:17 -0800

    F
    FYI

--- Biblioteca/App.xaml.cs
+++ Biblioteca/App.xaml.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Biblioteca
 {
-    /// <summary>
-    /// Lógica de interacción para App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Mostrar la ventana de Login al iniciar la aplicación
+            var loginWindow = new Views.LoginWindow();
+
+            // Mostrar la ventana de Login
+            if (loginWindow.ShowDialog() == true) // Solo procede si el login es exitoso
+            {
+                // Abrir la ventana principal si el login es válido
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                // Salir de la aplicación si el login falla o se cierra
+                Shutdown();
+            }
+        }
     }
 }
